05ac31327ae9726eafcc4cba36fbd66efaf3b84f
Abbey Gilliland <abbey.gilliland@ocsbstudent.ca>
2021-03-17 16:36:21 +0000

    fixed errors in Javascript

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ document.getElementById('show').addEventListener('click', show)
 document.getElementById('bg').addEventListener('click', backgroundcolor)
 // This code above me explains what each function will be named. Each button id has it's own function.
 function image () {
-  document.getElementById('budgies').src = "images/PaintAndArt.jpg"
+  document.getElementById('budgies').src = 'images/PaintAndArt.jpg'
 }
 // This code above me changes the picture of budgies into a picture of a paint palette.
 function text () {
-  document.getElementById('text').innerHTML = "What a nice photo!"
+  document.getElementById('text').innerHTML = 'What a nice photo!'
 }
 // This code above me changes the text by changing the innerHTML.
 function hide () {
-  document.getElementById('text').style.display = "none"
+  document.getElementById('text').style.display = 'none'
 }
 // This code above me hides the text if it hasn't already been hidden.
 function show () {
-  document.getElementById('text').style.display = "block"
+  document.getElementById('text').style.display = 'block'
 }
 // This code above shows the text it is hidden and gives it its own line.
 function backgroundcolor () {
-  document.body.style.backgroundColor = "mediumpurple"
+  document.body.style.backgroundColor = 'mediumpurple'
 }
 // This code above me changes the color to medium purple.
